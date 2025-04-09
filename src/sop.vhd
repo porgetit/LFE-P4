f7ccc70a9f976a02d8002715f0c1817dfadf754c
Kevin Esguerra Cardona <kevin.esguerra@utp.edu.co>
2025-04-09 08:02:07 -0500

    Update sop.vhd

--- src/sop.vhd
+++ src/sop.vhd
@@ -12,9 +12,15 @@ entity logic_function is
 end logic_function;
 
 architecture behavior of logic_function is
-    begin
-        S  <= ((not A3) and (not A2) and (not A1) and A0) or 
-        ((not A3) and A2 and (not A1) and (not A0)) or
-        (A3 and A2 and A1 and (not A0)) or 
-        (A3 and A2 and A1 and A0);
+
+signal w, y, x, z : std_logic;
+
+begin
+    
+w <= ((not A3) and (not A2) and (not A1) and A0);
+x <= ((not A3) and A2 and (not A1) and (not A0));
+y <= (A3 and A2 and A1 and (not A0));
+z <= (A3 and A2 and A1 and A0);
+S <= w or x or y or z;
+        
 end behavior;
