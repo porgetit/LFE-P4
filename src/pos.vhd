748886e048835d796c1e04844e4b596f269f4665
Kevin Esguerra Cardona <kevin.esguerra@utp.edu.co>
2025-04-09 08:09:09 -0500

    Update pos.vhd

--- src/pos.vhd
+++ src/pos.vhd
@@ -12,17 +12,21 @@ entity logic_function is
 end logic_function;
 
 architecture behavior of logic_function is
+
+signal o, p, q, r, s1, t, u, v, w, x, y, z : std_logic;
     begin
-        S <= (A3 or A2 or A1 or A0) and 
-        (A3 or A2 or (not A1) or A0) and 
-        (A3 or A2 or (not A1) or (not A0)) and 
-        (A3 or (not A2) or A1 or (not A0)) and 
-        (A3 or (not A2) or (not A1) or A0) and 
-        (A3 or (not A2) or (not A1) or (not A0)) and 
-        ((not A3) or A2 or A1 or A0) and 
-        ((not A3) or A2 or A1 or (not A0)) and 
-        ((not A3) or A2 or (not A1) or A0) and 
-        ((not A3) or A2 or (not A1) or (not A0)) and 
-        ((not A3) or (not A2) or A1 or A0) and 
-        ((not A3) or (not A2) or A1 or (not A0));
+    
+    o <= (A3 or A2 or A1 or A0);
+    p <= (A3 or A2 or (not A1) or A0);
+    q <= (A3 or A2 or (not A1) or (not A0));
+    r <= (A3 or (not A2) or A1 or (not A0));
+    s1 <= (A3 or (not A2) or (not A1) or A0);
+    t <= (A3 or (not A2) or (not A1) or (not A0));
+    u <= ((not A3) or A2 or A1 or A0);
+    v <= ((not A3) or A2 or A1 or (not A0));
+    w <= ((not A3) or A2 or (not A1) or A0);
+    x <= ((not A3) or A2 or (not A1) or (not A0));
+    y <= ((not A3) or (not A2) or A1 or A0);
+    z <= ((not A3) or (not A2) or A1 or (not A0));
+    S <= o and p and q and r and s1 and t and u and v and w and x and y and z;
 end behavior;
